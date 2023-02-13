a42d332c8c3d3e7f854106b5784d1780c4942fcf
ashishp911 <ashish.patil35@gmail.com>
2023-02-13 15:20:03 -0500

    ReactRouter added in app.js and about section adde

--- src/App.js
+++ src/App.js
@@ -1,56 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './MyComponents/Header.js';
-import {Todos} from './MyComponents/Todos'
-import Footer from './MyComponents/Footer'
-import AddTodo from './MyComponents/AddTodo'
-import React, { useState, useEffect } from 'react';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./MyComponents/Header.js";
+import { Todos } from "./MyComponents/Todos";
+import Footer from "./MyComponents/Footer";
+import AddTodo from "./MyComponents/AddTodo";
+import About from "./MyComponents/About"
+import React, { useState, useEffect } from "react";
+
+import {
+  createBrowserRouter as Router,
+  RouterProvider,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 function App() {
   let initTodo;
-  if(localStorage.getItem("todos") === null){
+  if (localStorage.getItem("todos") === null) {
     initTodo = [];
-  }
-  else{
+  } else {
     initTodo = JSON.parse(localStorage.getItem("todos"));
   }
   const onDelete = (todo) => {
     // console.log("I am Ondelete of ", todo)
-    setTodos(todos.filter((e) => {
-      return e !== todo
-    }));
+    setTodos(
+      todos.filter((e) => {
+        return e !== todo;
+      })
+    );
     localStorage.setItem("todos", JSON.stringify(todos));
-  }
-  
+  };
+
   const addTodo = (title, desc) => {
     let srno;
-    if(todos.length == 0){
-      srno = 0;  
+    if (todos.length == 0) {
+      srno = 0;
+    } else {
+      srno = todos[todos.length - 1].srno + 1;
     }
-    else{
-      srno = todos[todos.length-1].srno + 1
-    }
-    
+
     const mytodo = {
-      srno : srno,
-      title : title,
-      description : desc,
-    }
+      srno: srno,
+      title: title,
+      description: desc,
+    };
     console.log(mytodo);
-    setTodos([...todos,mytodo]);    
-  }
+    setTodos([...todos, mytodo]);
+  };
 
   const [todos, setTodos] = useState(initTodo);
   useEffect(() => {
     localStorage.setItem("todos", JSON.stringify(todos));
-  }, [todos])
+  }, [todos]);
 
   return (
     <>
-    <Header title = "MyTodosList" searchBar = {true}/>
-    <AddTodo addTodo = {addTodo}/>
-    <Todos todos = {todos} onDelete = {onDelete}/>
-    <Footer/>
+      <Router>
+        <Header title="MyTodosList" searchBar={true} />
+        <Switch>
+          <Route exact path="/" render = {() => {
+            return(
+              <>
+                <AddTodo addTodo={addTodo} />
+                <Todos todos={todos} onDelete={onDelete} />
+              </>
+            )
+          }}>
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
     </>
   );
 }
---
+++ src/MyComponents/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      This is an about component
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur veniam,
+        optio est fugiat deserunt dolores, et atque ipsam vero similique commodi
+        repellat, ex minus. Accusamus quisquam ut id eligendi est.
+      </p>
+    </div>
+  );
+};
+
+export default About;
